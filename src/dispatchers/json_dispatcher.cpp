6ab4682a7bd1c22ec974c192449055d7ac936b20
Catalyn45 <ancutei.catalin@gmail.com>
2022-06-02 19:25:13 +0300

    Made json dispatcher work

--- src/dispatchers/json_dispatcher.cpp
+++ src/dispatchers/json_dispatcher.cpp
@@ -1,16 +1,45 @@
 #include "json_dispatcher.h"
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include "../utils/logging.h"
 
 static Logger* logger = Logger::get_logger();
+
+using json = nlohmann::json;
 
 JsonDispatcher::JsonDispatcher(Engine* engine, const std::string path)
     : Dispatcher(engine, "Json dispatcher"), path(std::move(path)) {}
 
 void JsonDispatcher::dispatch(Event* event) {
-    json j = event->serialize();
-    logger->detection(std::string(j["id"]).c_str());
-    // std::ofstream f(path);
-    // f << j << std::endl;
+    json j_event = event->serialize();
+    logger->detection(std::string(j_event["id"]).c_str());
+
+    std::ofstream tmp(this->path, std::ios::app);
+    tmp.close();
+
+    std::fstream f(this->path, std::fstream::in | std::fstream::out);
+    if (!f.is_open()) {
+        logger->error("error opening file: %s", this->path.c_str());
+        return;
+    }
+
+    std::stringstream content;
+    content << f.rdbuf();
+
+    if (content.str().find("[") == std::string::npos) {
+        content.str("[]");
+    }
+
+    f.seekg(0, std::ios::beg);
+
+    json j_detections = json::parse(content.str());
+    j_event["dispatched_time"] = std::time(nullptr);
+    j_detections.push_back(j_event);
+
+    logger->detection("%s", j_detections.dump(4).c_str());
+
+    f << j_detections.dump(4);
+
+    f.close();
 }
