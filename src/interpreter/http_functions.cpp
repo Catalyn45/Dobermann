44ed16d149a78b01cb3ad00940a65a07d6a78609
Catalyn45 <ancutei.catalin@gmail.com>
2022-06-05 23:26:09 +0300

    Renamed contains function

--- src/interpreter/http_functions.cpp
+++ src/interpreter/http_functions.cpp
@@ -4,19 +4,23 @@
 #include <string>
 #include "../utils/packets.h"
 
-static int header_contains(const vm_args_t& script_args, const json& args) {
+static int http_packet_contains(const vm_args_t& script_args, const json& args) {
     HttpPacket* packet = (HttpPacket*)script_args.at("packet");
     std::vector<std::string> tokens = args["tokens"];
 
     for (auto& token : tokens) {
-        if (packet->path.find(token.c_str()) != std::string::npos) {
+        if (packet->path.find(token) != std::string::npos) {
             return 0;
         }
 
         for (auto& header : packet->headers) {
-            if (header.second.find(token.c_str()) != std::string::npos) {
+            if (header.second.find(token) != std::string::npos) {
                 return 0;
             }
+        }
+
+        if (packet->body.find(token) != std::string::npos) {
+            return 0;
         }
     }
 
@@ -24,5 +28,5 @@ static int header_contains(const vm_args_t& script_args, const json& args) {
 }
 
 vm_functions_t http_functions_map = {
-    {"header_contains", header_contains}
+    {"http_packet_contains", http_packet_contains}
 };
