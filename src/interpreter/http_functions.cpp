01ada279f2a976cddfa2d2bceb1066863b50874e
Catalyn45 <ancutei.catalin@gmail.com>
2022-06-05 23:36:13 +0300

    Added more http functions for signatures

--- src/interpreter/http_functions.cpp
+++ src/interpreter/http_functions.cpp
@@ -27,6 +27,62 @@ static int http_packet_contains(const vm_args_t& script_args, const json& args) 
     return -1;
 }
 
+static int http_path_contains(const vm_args_t& script_args, const json& args) {
+    HttpPacket* packet = (HttpPacket*)script_args.at("packet");
+    std::vector<std::string> tokens = args["tokens"];
+
+    for (auto& token : tokens) {
+        if (packet->path.find(token) != std::string::npos) {
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+static int http_header_contains(const vm_args_t& script_args, const json& args) {
+    HttpPacket* packet = (HttpPacket*)script_args.at("packet");
+    std::vector<std::string> tokens = args["tokens"];
+
+    for (auto& header : packet->headers) {
+        for (auto& token : tokens) {
+            if (header.second.find(token) != std::string::npos) {
+                return 0;
+            }
+        }
+    }
+
+    return -1;
+}
+
+static int http_body_contains(const vm_args_t& script_args, const json& args) {
+    HttpPacket* packet = (HttpPacket*)script_args.at("packet");
+    std::vector<std::string> tokens = args["tokens"];
+
+    for (auto& token : tokens) {
+        if (packet->body.find(token) != std::string::npos) {
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+static int http_method_is(const vm_args_t& script_args, const json& args) {
+    HttpPacket* packet = (HttpPacket*)script_args.at("packet");
+    std::string method = args["method"];
+
+    if (packet->method == method) {
+        return 0;
+    }
+
+    return -1;
+}
+
 vm_functions_t http_functions_map = {
-    {"http_packet_contains", http_packet_contains}
+    {"http_packet_contains", http_packet_contains},
+    {"http_path_contains", http_path_contains},
+    {"http_header_contains", http_header_contains},
+    {"http_body_contains", http_body_contains},
+    {"http_method_is", http_method_is}
 };
